08789c3d3d145d9e4400c654bd279699118b206a
adrien morvan <adrien.morvan@laposte.fr>
2022-01-28 11:33:15 +0100

    correct number of message

--- server/src/main/java/fr/lernejo/chat/ChatMessageRepository.java
+++ server/src/main/java/fr/lernejo/chat/ChatMessageRepository.java
@@ -10,7 +10,7 @@ public class ChatMessageRepository {
     List<String> a= new ArrayList<>();
     void addChatMessage (String message) {
         a.add(message);
-        if (a.size() >= 10)
+        if (a.size() > 10)
             a.remove(0);
     }
     List<String> getLastTenMessages() {
